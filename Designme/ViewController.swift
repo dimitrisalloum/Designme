638e04b15c6b4c99428c04366d2093eb9aff7425
karen <karensaleh777@gmail.com>
2018-02-25 17:06:20 +0200

    added a message

--- Designme/ViewController.swift
+++ Designme/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //Dimitri was here
+        print("hey its karen")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
